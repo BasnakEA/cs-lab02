e73fb688b547e90aa6e618487604af8ebfa3c266
Bob (BasnakEA) <bob@example.com>
2020-03-13 12:12:16 +0300

    search of minimum

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-    int a, b, max;
+    int a, b, max, min;
     cin >> a >> b;
 
     max = a + b;
@@ -16,12 +16,22 @@ int main()
     if (a / b > max)
         max = a / b;
 
+
+     min = a + b;
+    if ((a - b) < min)
+        min = a - b;
+    if (a * b < min)
+        min = a* b;
+    if (a / b < min)
+        min = a / b;
+
+
     cout << "A + B = " << a + b << '\n'
     << "A - B = " << a - b << '\n'
     << "A * B = " << a * b << '\n'
     << "A / B = " << a / b << '\n'
-    << "max = " << max << '\n';
-
+    << "max = " << max << '\n'
+    << "min = " << min << '\n';
 
 
     return 0;
